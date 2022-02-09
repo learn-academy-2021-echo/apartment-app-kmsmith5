bf1219dc65a35c9bf9ca4353cdc5626a97c8be9f
kmsmith5 <kmsmith5@gmail.com>
2022-02-09 17:01:10 -0500

    adding change to App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
     return(
     <>
     <h1>Hello World</h1>
